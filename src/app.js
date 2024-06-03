2bb0a5edc5b5e41559af8b654b25980abba34e35
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-06-03 23:53:19 +0000

    in progress

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const whos = ["The dog", "My grandma", "The mailman", "My bird"];
+  const actions = ["ate", "peed", "crushed", "broke"];
+  const whats = ["my homework", "my phone", "the car"];
+  const whens = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whosIndex = Math.trunc(Math.random() * whos.length);
+  const who = whos[whosIndex];
+  const actionsIndex = Math.trunc(Math.random() * actions.length);
+  const action = actions[actionsIndex];
+  const whatsIndex = Math.trunc(Math.random() * whats.length);
+  const what = whos[whosIndex];
+  const whensIndex = Math.trunc(Math.random() * whens.length);
+  const when = whens[whensIndex];
+  const excuse = `${who} ${action} ${what} ${when}`;
   console.log("Hello Rigo from the console!");
 };
